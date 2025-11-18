4b98cbd9b03ae9bddd9a1d559a835171f545ec4c
Ben Cöppicus <ben.coeppicus@bencoepp.de>
2025-11-18 14:42:00 +0100

    Enhance VM module: add SSH key management, update security group rules, and define outputs for instance details

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -11,8 +11,8 @@ resource "aws_vpc_security_group_ingress_rule" "ingress_rules" {
     for_each = { for idx, rule in var.ingress_rules : idx => rule }
     
     security_group_id = aws_security_group.security_group.id
-    from_port         = each.value.from_port
-    to_port           = each.value.to_port
+    from_port         = each.value.protocol == "-1" ? null : each.value.from_port
+    to_port           = each.value.protocol == "-1" ? null : each.value.to_port
     ip_protocol       = each.value.protocol
     cidr_ipv4         = each.value.cidr_ipv4
 }
@@ -21,8 +21,8 @@ resource "aws_vpc_security_group_egress_rule" "egress_rules" {
     for_each = { for idx, rule in var.egress_rules : idx => rule }
     
     security_group_id = aws_security_group.security_group.id
-    from_port         = each.value.from_port
-    to_port           = each.value.to_port
+    from_port         = each.value.protocol == "-1" ? null : each.value.from_port
+    to_port           = each.value.protocol == "-1" ? null : each.value.to_port
     ip_protocol       = each.value.protocol
     cidr_ipv4         = each.value.cidr_ipv4
 }
